60663adc18b8d390e1462702cdd8b4c35bdb96f9
RIPI80 <100714811+RIPI80@users.noreply.github.com>
2022-06-16 19:47:52 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  var element = document.getElementById("pink");
+  document.body.style.backgroundColor="pink";
+}
+
+function myFunction1() {
+  var element = document.getElementById("yellow");
+  document.body.style.backgroundColor="yellow";
+}
+
+function myFunction2() {
+  var element = document.getElementById("purple");
+  document.body.style.backgroundColor="purple";
+}
+
+function myFunction3() {
+  var element = document.getElementById("blue");
+  document.body.style.backgroundColor="blue";
+}
